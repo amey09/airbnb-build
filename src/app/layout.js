fded55cbe5325aa9afac9c360e0919efad610bd6
amey09 <ameymhatre09@gmail.com>
2023-10-01 13:25:26 +0530

    fix/copy-changes-in-layout-jsx

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
-import { Providers } from "./Providers";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Header from "./components/Header";
+import { Providers } from "./providers";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
